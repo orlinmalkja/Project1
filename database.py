f0efdff2f4aa8d1531b3bc63c6df901b10fb40f4
Mariza Mehmet <mehmetmarizatik@gmail.com>
2020-06-25 11:02:42 +0000

    I added database.py also the user class with getters and setters

---
+++ database.py
@@ -0,0 +1,114 @@
+import os
+from os import path
+
+class Database:
+    def __init__(self,dbname,dbpath="."):
+        # set the destination path for the database folder
+        self.mainpath = dbpath+path.sep+dbname
+        if(not path.exists(self.mainpath)):
+            # if folder does not exists create a new one
+            os.mkdir(self.mainpath)
+        # create a dictionary that will hold tables
+        self.tables = {}
+
+    # creates a file if it does not exist
+    def createtableifnotexists(self,tname,ttype,toObject):
+        # check if is created before
+        if(not path.exists(self.mainpath+path.sep+tname)):
+            # add specific entry into the dictionary
+            self.tables[tname]=Table(self.mainpath+path.sep+tname,ttype,toObject)
+            # create respective file
+            fd = open(self.mainpath+path.sep+tname,"w")
+            fd.close()
+        else:
+            # scince the file already exists just add the entry into the dictionary
+            self.tables[tname]=Table(self.mainpath+path.sep+tname,ttype,toObject)
+
+    # get a list of objects from a specific table. you can also filter by a lambda function
+    def getObjectsFrom(self,tname,condition=lambda a:True):
+        # find table data from the dictionary
+        table = self.tables[tname]
+        # open the file
+        with (open(table.getpath(),"r")) as fp:
+            records = []
+            # read all lines
+            lines = fp.readlines()
+            for line in lines:
+                # construct the object by the specific function
+                record = table.toObject(line)
+                # if object fullfill the condition then add into list
+                print("testing",condition(record))
+                if(condition(record)):
+                    records.append(record)
+            # close the file and then return the ist of selected objects
+            fp.close()
+            return records
+        return(101,"file not found")
+
+    # this method just adds the objects in the end of the file
+    def appendObjectsInto(self,tname,records):
+        table = self.tables[tname]
+        # after taking table data open file
+        with open(table.getpath() ,"a") as fp:
+            for  record in records:
+                # if object is of specififed type
+                if table.checkType(record):
+                    # write it into the file
+                    fp.write(record.toString()+"\n")
+            fp.close()
+
+    def appendObjectInto(self,tname,obj):
+        self.appendObjectsInto(tname,[obj])
+    # this function deletes records specifying a specific function
+    def deleteObjectsFrom(self,tname,condition=lambda x:False):
+        # select the respective table from dictionary
+        table = self.tables[tname]
+        # read all records
+        with (open(table.getpath(),"r")) as fp:
+            # read all lines of data one line for one object
+            lines = fp.readlines()
+            fp.close()
+        # write onnly those that do not fullfill the condition
+        with(open(table.getpath(),"w") )as fp:
+            # for each line
+            for line in lines:
+                # construct the apropriate object using the
+                # function toObject of the respective table
+                record = table.toObject(line)
+                if(not condition(record)):
+                    fp.write(line)
+            fp.close()
+        return(101,"file not found")
+
+    # this function writes objects in a file but the
+    # old data is always lost
+    def overwriteObjectsInto(self,tname,newobjects):
+        # select the table
+        table = self.tables[tname]
+        # open the file
+        with open(table.getpath() ,"w") as fp:
+            # for each object
+            for  newobject in newobjects:
+                # check if it is an apropriate type for this table
+                if table.checkType(newobject):
+                    # dump the record into the table
+                    fp.write(newobject.toString()+"\n")
+            fp.close()
+
+class Table :
+    # a table is an instance that is defined by
+    # a path where the objects are stored (lines)
+    # a type stating types of objects stored in file
+    # a toObject method that converts a line into an object
+    # of specified type
+    def __init__(self,tpath,ttype,toObject):
+        self.__tpath = tpath
+        self.__type = ttype
+        self.__toObject = toObject
+
+    # table destination in file system
+    def getpath(self):return self.__tpath
+    # checks if an object is of table specified type
+    def checkType(self,ttype):return self.__type==type(ttype)
+    # returns a line into an object of specified type of table
+    def toObject(self,line):return self.__toObject(line)
--- user.py
+++ user.py
@@ -1,13 +1,58 @@
 class User:
-  def __init__(self,id,name):
-    self.id = id
+  def __init__(self, ID, email, password,name,surname,age):
+    self.id = ID
+    self.email = email
+    self.password = password
     self.name = name
+    self.surname = surname
+    self.age = age
+    self.bills = []
+
+
+  @classmethod
+  def fromstring(cls,x):
+    _ID,_email,_password, _name, _surname, _age = x.split("-")
+    return cls(_ID,_email,_password,_name, _surname,_age)
+
+#getters
+
+  def getID(self):
+    return self.id
+
+  def getEmail(self):
+   return self.email
+
+  def getPassword(self):
+    return self.password
 
   def getName(self):
     return self.name
 
-  def setName(self,newName):
-      self.name = newName
+  def getSurname(self):
+    return self.surname
 
-  def toString(self):
-    print("User with id",self.id,"and name",self.name)
+  def getAge(self):
+    return self.age
+
+
+
+#setters 
+
+  def setID(self,ID):
+    self.id = ID
+
+  def setEmail(self,email):
+    self.email = email
+
+  def setPassword(self,password):
+    self.password = password
+
+  def setName(self,name):
+    self.name = name
+
+  def setSurname(self,surname):
+    self.surname = surname
+
+  def setAge(self,age):
+    self.age = age
+
