61e7a3d98f361195b03642ffefcac6825506437c
Klovis Karoshi <kloviskaroshi@gmail.com>
2020-07-05 10:37:38 +0000

    added Status variable to User class, will be used to determine whether a user is an admin or a normal user

--- user.py
+++ user.py
@@ -1,18 +1,20 @@
 class User:
-  def __init__(self, ID, email, password,name,surname,age):
+  def __init__(self, ID, email, password,name,surname,age,status):
     self.id = ID
     self.email = email
     self.password = password
     self.name = name
     self.surname = surname
     self.age = age
+    self.status = status
+    #status do jet "admin" ose "user" si string, mund edhe ta bejme True ose False po nuk ka rendesi
     self.bills = []
 
 
   @classmethod
   def fromstring(cls,x):
-    _ID,_email,_password, _name, _surname, _age = x.split("-")
-    return cls(_ID,_email,_password,_name, _surname,_age)
+    _ID,_email,_password, _name, _surname, _age, _status = x.split("-")
+    return cls(_ID,_email,_password,_name, _surname,_age, _status)
 
 #getters
 
@@ -34,6 +36,8 @@ class User:
   def getAge(self):
     return self.age
 
+  def getStatus(self):
+    return self.status
 
 
 #setters 
@@ -56,4 +60,6 @@ class User:
   def setAge(self,age):
     self.age = age
 
+  def setStatus(self, status):
+    self.status = status
  
