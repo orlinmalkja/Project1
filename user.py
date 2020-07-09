e2a57747d6c167c735a60513ef3ae750ce30ef17
Klovis Karoshi <kloviskaroshi@gmail.com>
2020-07-09 17:19:49 +0000

    added changes to user class

--- user.py
+++ user.py
@@ -1,17 +1,18 @@
 from date import Date
 class User:
-    def __init__(self,id,name,bdate,email,passw,address):
+    def __init__(self,id,name,bdate,email,passw,address,status):
         self._id = id
         self._name = name
         self._bdate = bdate
         self._email = email
         self._passw = passw
         self._address = address
+        self._status = status
 
 
 
     def toString(self):
-        return str(self._id)+","+self._name+","+self._bdate.toString()+","+str(self._email)+","+str(self._passw)+","+self._address
+        return str(self._id)+","+self._name+","+self._bdate.toString()+","+str(self._email)+","+str(self._passw)+","+self._address+ ","+ self._status
 
 
     @classmethod
@@ -22,9 +23,13 @@ class User:
         bdate = Date.fromString(fields[2])
         email = fields[3]
         passw = fields[4]
-        address = (fields[5])
-        return cls(id,name,bdate,email,passw,address)
+        address = fields[5]
+        status = fields[6]
+        return cls(id,name,bdate,email,passw,address,status)
 
 
     def getName(self):
         return self._name
+
+    def getStatus(self):
+        return self._status
